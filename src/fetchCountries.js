147bf75a8051e4bbfd57098b238b3c647cc2e01c
Trifonov Denys <trifonowden123@gmail.com>
2023-05-21 23:42:58 +0300

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,4 +1,4 @@
-export default function fetchCountries(name) {
+export function fetchCountries(name) {
   const BASE_URL = 'https://restcountries.com/v3.1';
 
   return fetch(`${BASE_URL}/name/${name}?fields=flags,name,capital,population,languages`)
